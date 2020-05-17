ea30425a4c2e16b46e97883c610bab9d668589c0
Rodrigo Crispim <crispim1411@gmail.com>
2020-05-17 02:00:18 -0300

    Verificação versão do Java

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
+# check java version
+version=$(java -version 2>&1 | awk -F\" '{ split($2,a,"."); print a[1]}')
+    if [[ $version -ge "8" ]]; then
+        echo "Java version OK"
+    else
+        echo "INVALID version of Java $version. Must be Java 8+"
+    fi
+
 # check python version
 version=$(python -V 2>&1 | grep -Po '(?<=Python )(.+)')
 parsedVersion=$(echo "${version//./}")
 if [[ "$parsedVersion" -gt "370" ]]
 then
-    echo "Valid version $version"
+    echo "Python version OK"
 else
     echo "INVALID version $version"
 fi
@@ -17,6 +25,6 @@ else
   python -m venv venv
 fi
 
-# installing python's dependencies
+# installing python dependencies
 source venv/bin/activate
 pip install -r requirements.txt
